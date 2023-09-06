fd340803a2b7f5aa4b853f4281793b037900eeb3
Volodymyr Ivanchenko <vladymyrivanchenko3@gmail.com>
2023-09-06 20:30:40 +0300

    fix: move context state logic into reducer

---
+++ context/constants.js
@@ -0,0 +1,2 @@
+export const DELETE_POST = 'deletePost';
+export const ADD_POSTS = 'addPosts';
--- context/postContext.js
+++ context/postContext.js
@@ -1,35 +1,47 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useReducer, useState } from 'react';
+import * as ACTIONS from './constants';
 
 const PostContext = React.createContext({});
 
 export default PostContext;
 
-export const PostsProvider = ({children}) => {
-    const [posts, setPosts] = useState([]);
-    const [noMorePosts, setNoMorePosts] = useState(false);
-
-    const deletePost = useCallback((postId) => {
-        setPosts(postsArray => {
-            const results = [];
-            postsArray.forEach(post => {
-                if (post._id !== postId) {
-                    results.push(post);
-                }
-            });
-            return results;
-        });
-    }, []);
-
-    const setPostsFromSSR = useCallback((postsFromSSR = []) => {
-        setPosts(value => {
-            const newPosts = [...value];
-            postsFromSSR.forEach(post => {
+const postsReducer = (state, {type, posts = [], postId}) => {
+    switch(type) {
+        case ACTIONS.ADD_POSTS: {
+            const newPosts = [...state];
+            posts.forEach(post => {
                 const exists = newPosts.find((p) => p._id === post._id)
                 if (!exists) newPosts.push(post)
             });
     
             return newPosts;
-        });
+        }
+
+        case ACTIONS.DELETE_POST: {
+            const results = [];
+            state.forEach(post => {
+                if (post._id !== postId) {
+                    results.push(post);
+                }
+            });
+            return results;
+        }
+
+        default: 
+            return state;
+    }
+}
+
+export const PostsProvider = ({children}) => {
+    const [posts, dispatch] = useReducer(postsReducer, []);
+    const [noMorePosts, setNoMorePosts] = useState(false);
+
+    const deletePost = useCallback((postId) => {
+        dispatch({type: ACTIONS.DELETE_POST, postId});
+    }, []);
+
+    const setPostsFromSSR = useCallback((postsFromSSR = []) => {
+        dispatch({type: ACTIONS.ADD_POSTS, posts: postsFromSSR});
     }, []);
 
     const getPosts = useCallback(async ({lastPostDate, getNewerPosts = false}) => {
@@ -48,16 +60,7 @@ export const PostsProvider = ({children}) => {
             setNoMorePosts(true);
         }
 
-        setPosts(value => {
-            const newPosts = [...value];
-            postsResult.forEach(post => {
-                const exists = newPosts.find((p) => p._id === post._id)
-                if (!exists) newPosts.push(post)
-            });
-    
-            return newPosts;
-        });
-
+        dispatch({type: ACTIONS.ADD_POSTS, posts: postsResult});
     }, []);
 
     return <PostContext.Provider value={{posts, setPostsFromSSR, getPosts, noMorePosts, deletePost}}>{children}</PostContext.Provider>
